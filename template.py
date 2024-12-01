18085fd8afb16c284b2f542e3c05b1bc84956d83
giganizer <ziga.bradas@gmail.com>
2024-12-01 17:11:37 +0100

    Added a solution template

---
+++ template.py
@@ -0,0 +1,37 @@
+# Advent of Code 2025
+# Day 
+# 
+####################################################################
+
+# imports
+
+
+# reading the puzzle input
+
+# SET PATH HERE
+####################################
+file_path = "" # path to the input file
+####################################
+
+# PREPARE CONTENT VARS HERE
+####################################
+
+####################################
+
+try:
+    with open(file_path, 'r') as file:
+
+        # PROCESS THE FILE CONTENTS HERE
+        ########################################
+
+        
+
+        ########################################
+
+except FileNotFoundError:
+    print(f"Error: File not found at {file_path}")
+except Exception as e:
+    print(f"An error occurred while reading the file: {e}")
+
+
+# puzzle solution
