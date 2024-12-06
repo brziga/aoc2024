53ea3c1ffa17eb274d93324fd9740e41c5061a09
giganizer <ziga.bradas@gmail.com>
2024-12-06 20:47:47 +0100

    Day 6, part 2 solution

--- 6/day6.py
+++ 6/day6.py
@@ -4,7 +4,7 @@
 ####################################################################
 
 # imports
-
+import copy
 
 # reading the puzzle input
 
@@ -15,7 +15,7 @@ file_path = "6/day6_input1.txt" # path to the input file
 
 # PREPARE CONTENT VARS HERE
 ####################################
-area_map = []
+area = []
 ####################################
 
 try:
@@ -25,7 +25,7 @@ try:
         ########################################
 
         for line in file:
-            area_map.append(list(line.strip()))
+            area.append(list(line.strip()))
 
         ########################################
 
@@ -38,28 +38,32 @@ except Exception as e:
 # puzzle solution
 
 # Part 1
+area_map = copy.deepcopy(area)
 m, n = len(area_map), len(area_map[0])
-# vm, hm = {}, {} # vertical and horizontal mappings
 guard_orients = {"^", "<", "v", ">"}
-guard_pos = None
+initial_guard_pos = None
 
 for y in range(m):
     for x in range(n):
         if area_map[y][x] in guard_orients:
-            guard_pos = (y, x)
+            initial_guard_pos = (y, x)
             break
-    if guard_pos: break
+    if initial_guard_pos: break
 
 directions = {"^": (-1, 0), "<": (0, -1), "v": (1, 0), ">": (0, 1)}
 in_area = True
 visited_count = 0
-guard = area_map[y][x]
+turns = set()
+visited = set()
+guard = area_map[initial_guard_pos[0]][initial_guard_pos[1]]
+guard_pos = initial_guard_pos
 while in_area:
     # move the guard
     y, x = guard_pos
     while 0 <= y < m and 0 <= x < n and area_map[y][x] != "#":
         if area_map[y][x] != "X": visited_count += 1
         area_map[y][x] = "X"
+        visited.add((y, x))
         guard_pos = (y, x)
         y += directions[guard][0]
         x += directions[guard][1]
@@ -69,6 +73,7 @@ while in_area:
         break
     else:
         # hit an obstacle
+        turns.add((y, x))
         match guard:
             case "^":
                 guard = ">"
@@ -80,3 +85,48 @@ while in_area:
                 guard = "^"
 
 print(f"The guard will visit {visited_count} distinct positions before leaving the area")
+
+# Part 2
+obstructions = [] # possible positions for obstructions
+visited.remove(initial_guard_pos) # remove the guard's initial position
+visit_iter = iter(visited)
+for ypos, xpos in visit_iter:
+    # place obstruction
+    obstructed_area = copy.deepcopy(area)
+    obstructed_area[ypos][xpos] = "#"
+    # simulate guard movement and detect a loop
+    in_area = True
+    new_turns = set()
+    guard_pos = initial_guard_pos
+    guard = area[initial_guard_pos[0]][initial_guard_pos[1]]
+    while in_area:
+        y, x = guard_pos
+        while 0 <= y < m and 0 <= x < n and obstructed_area[y][x] != "#":
+            obstructed_area[y][x] = "X"
+            guard_pos = (y, x)
+            y += directions[guard][0]
+            x += directions[guard][1]
+        if y < 0 or y >= m or x < 0 or x >= n:
+            # area escaped
+            in_area = False
+            break
+        else:
+            # hit an obstacle
+            if (guard_pos[0], guard_pos[1], guard) in new_turns:
+                # loop detected
+                break
+            else:
+                new_turns.add((guard_pos[0], guard_pos[1], guard))
+            match guard:
+                case "^":
+                    guard = ">"
+                case ">":
+                    guard = "v"
+                case "v":
+                    guard = "<"
+                case "<":
+                    guard = "^"
+    if in_area:
+        obstructions.append((ypos, xpos))
+
+print(f"{len(obstructions)} positions can be chosen for the obstruction")
